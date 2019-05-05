17124cc2396560351ca33c75c1f76b96d58fe719
Edvard-Hagerup-Grieg <mari.louis.von.franz@gmail.com>
2019-05-05 11:54:33 +0300

    parallel omp

--- ParProg/ParProg/main.cpp
+++ ParProg/ParProg/main.cpp
@@ -35,11 +35,45 @@ double CannonAlgorithm(double * AM, double * BM, double * CM, int N, int B)
 	return (finish - start);
 }
 
+double CannonAlgorithm_OMP(double * AM, double * BM, double * CM, int N, int B)
+{
+	double start = 0, finish = 0;
+
+	start = omp_get_wtime();
+#pragma omp parallel for
+	for (int block = 0; block < B*B; block++)
+	{
+		int ib = block / B;
+		int jb = block - ib * B;
+
+		int jj = (ib + jb) % B;
+
+		for (int k = 0; k < B; k++)
+		{
+			for (int mi = ib * (N / B); mi < (ib + 1)*(N / B); mi++)
+				for (int mj = jb * (N / B); mj < (jb + 1)*(N / B); mj++)
+				{
+					double SUM = 0.0;
+					for (int mk = jj * (N / B); mk < (jj + 1)*(N / B); mk++)
+						SUM += AM[mi*N + mk] * BM[mk*N + mj];
+					CM[mi*N + mj] += SUM;
+				}
+
+			jj = (jj + 1) % B;
+		}
+	}
+	finish = omp_get_wtime();
+
+	return (finish - start);
+}
+
+
 
 void main()
 {
-	int B = 3;			//grid size B*B
-	int N = B * 2;		//matrix size: N*N, block size 2*2
+	int B = 10;			// grid size B*B
+	int b = 50;			// block size b*b
+	int N = B * b;		// matrix size: N*N
 
 	double *AM = new double[N*N];
 	double *BM = new double[N*N];
@@ -53,10 +87,10 @@ void main()
 	}
 
 	double TIME = 0.0;
-	TIME = CannonAlgorithm(AM, BM, CM, N, B);
+	TIME = CannonAlgorithm_OMP(AM, BM, CM, N, B);
 	cout << "TIME: " << TIME << endl;
 
-	bool visual = true;
+	bool visual = false;
 	if (visual) 
 	{
 		cout << "A:" << endl;
