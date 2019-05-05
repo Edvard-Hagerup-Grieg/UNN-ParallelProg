4737874442e7cf60cad4f98fa53ade2ce05552fb
Edvard-Hagerup-Grieg <mari.louis.von.franz@gmail.com>
2019-05-05 13:12:15 +0300

    parallel tbb

--- ParProg/ParProg/main.cpp
+++ ParProg/ParProg/main.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
 #include "omp.h"
+#include "tbb/task_scheduler_init.h"
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
 
 
 using namespace std;
+using namespace tbb;
 
-double CannonAlgorithm(double * AM, double * BM, double * CM, int N, int B)
+
+class CannonAlgorithm
+{
+	const double *AM, *BM;
+	double *const CM;
+	int const N;
+	int const B;
+
+public:
+	CannonAlgorithm(double *AM, double *BM, double *CM, int N, int B) :
+		AM(AM), BM(BM), CM(CM), N(N), B(B) {}
+
+	void operator()(const blocked_range<int>& r) const
+	{
+		int begin = r.begin(), end = r.end();
+		for (int block = begin; block != end; block++)
+		{
+			int ib = block / B;
+			int jb = block - ib * B;
+
+			int jj = (ib + jb) % B;
+
+			for (int k = 0; k < B; k++)
+			{
+				for (int mi = ib * (N / B); mi < (ib + 1)*(N / B); mi++)
+					for (int mj = jb * (N / B); mj < (jb + 1)*(N / B); mj++)
+					{
+						double SUM = 0.0;
+						for (int mk = jj * (N / B); mk < (jj + 1)*(N / B); mk++)
+							SUM += AM[mi*N + mk] * BM[mk*N + mj];
+						CM[mi*N + mj] += SUM;
+					}
+
+				jj = (jj + 1) % B;
+			}
+		}
+	}
+};
+
+double CannonAlgorithm_Seq(double * AM, double * BM, double * CM, int N, int B)
 {
 	double start = 0, finish = 0;
 	
@@ -67,11 +110,22 @@ double CannonAlgorithm_OMP(double * AM, double * BM, double * CM, int N, int B)
 	return (finish - start);
 }
 
+double CannonAlgorithm_TBB(double * AM, double * BM, double * CM, int N, int B, int grainSize)
+{
+	task_scheduler_init init;
+	double start = 0, finish = 0;
+
+	start = omp_get_wtime();
+	parallel_for(blocked_range<int>(0, B*B, grainSize), CannonAlgorithm(AM, BM, CM, N, B));
+	finish = omp_get_wtime();
+
+	return (finish - start);
+}
 
 
 void main()
 {
-	int B = 10;			// grid size B*B
+	int B = 40;			// grid size B*B
 	int b = 50;			// block size b*b
 	int N = B * b;		// matrix size: N*N
 
@@ -87,8 +141,16 @@ void main()
 	}
 
 	double TIME = 0.0;
+	cout << "SIZE: " << N << endl;
+
+	TIME = CannonAlgorithm_Seq(AM, BM, CM, N, B);
+	cout << "TIME Seq: " << TIME << endl;
+
 	TIME = CannonAlgorithm_OMP(AM, BM, CM, N, B);
-	cout << "TIME: " << TIME << endl;
+	cout << "TIME OMP: " << TIME << endl;
+
+	TIME = CannonAlgorithm_TBB(AM, BM, CM, N, B, 1);
+	cout << "TIME TBB: " << TIME << endl;
 
 	bool visual = false;
 	if (visual) 
